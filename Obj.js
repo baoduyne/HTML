3bb143eb65d4aa4e9363713436ed105d16bfcb71
sansukai <lobaoduy2017@gmail.com>
2024-07-01 10:28:55 +0700

    update Obj.html and Obj.js

--- Obj.js
+++ Obj.js
@@ -7,5 +7,17 @@ const obj = {
     }
 }
 
+const student = {
+    Name : "",
+    id : 0,
+
+    add : function(name,id){
+        this.Name = name;
+        this.id = id;
+        return this.Name + this.id;
+    }
+}
 
 document.getElementById("id1").innerHTML = obj.Note();
+
+document.getElementById("id2").innerHTML = student.add("Duy",5);
